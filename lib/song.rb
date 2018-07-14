577f996d07a8549b646a71782989429a0762db2d
Michael Morris <mdam2345@gmail.com>
2018-07-14 02:50:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,7 @@ require_relative './concerns/paramable'
 
 class Song
   extend Memorable::All
+    include Memorable::InstanceMethods
   extend Findable::All
   include Paramable::All
   attr_accessor :name
