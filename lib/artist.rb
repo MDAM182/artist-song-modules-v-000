6e8176f6462e3911201c44273a14775602b8c7e3
Michael Morris <mdam2345@gmail.com>
2018-07-14 02:49:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ require_relative './concerns/paramable'
 
 class Artist
   extend Memorable::All
-  include Memorable::  InstanceMethods
+  include Memorable::InstanceMethods
   extend Findable::All
   include Paramable::All
   attr_accessor :name
