3d80dbc2a0855bf554db4f9961cf4c2f37d7f4dc
Michael Morris <mdam2345@gmail.com>
2018-07-14 02:19:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 require_relative './concerns/memorable'
 
 class Song
-  
+
   extend Memorable::All
   attr_accessor :name
   attr_reader :artist
@@ -13,9 +13,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
